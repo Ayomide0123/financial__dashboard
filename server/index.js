2650eb7c9c50eac8c05f66f5e50a3a808de7f664
Ayomide <oyetimehin31@gmail.com>
2025-02-05 15:08:27 +0100

    update vercel.json

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5001;
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -17,4 +19,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/financial", financialRoutes);
 
 // Vercel requires the export to be a handler
-export default (req, res) => app(req, res);
+// export default (req, res) => app(req, res);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;

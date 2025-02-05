d2b54316f719faa491f3c9389ea15d02a7d7f94b
Ayomide <oyetimehin31@gmail.com>
2025-02-05 14:51:01 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes.js"
-import financialRoutes from "./routes/financialRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
+import financialRoutes from "./routes/financialRoutes.js";
 
 dotenv.config();
 
 const app = express();
-// const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
@@ -17,7 +16,5 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/financial", financialRoutes);
 
-// No need to start the server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+// Vercel requires the export to be a handler
+export default (req, res) => app(req, res);
